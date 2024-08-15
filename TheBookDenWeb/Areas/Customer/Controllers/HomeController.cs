1309230a169882f1181842b5b872c05ec7ed47dd
chamara <chamara_sachintha@yahoo.com>
2024-08-15 15:03:49 +1000

    Home page completed

--- TheBookDenWeb/Areas/Customer/Controllers/HomeController.cs
+++ TheBookDenWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using TheBookDen.DataAccess.Repository.IRepository;
 using TheBookDen.Models.Models;
 
 namespace TheBookDenWeb.Areas.Customer.Controllers;
@@ -7,15 +8,24 @@ namespace TheBookDenWeb.Areas.Customer.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IUnitOfWork _unitOfWork;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
     {
-        _logger = logger;
+        _logger = logger;   
+        _unitOfWork = unitOfWork;
     }
 
     public IActionResult Index()
     {
-        return View();
+        IEnumerable<Product> productList = _unitOfWork.ProductRepository.GetAll(includeProperties:"Category");
+        return View(productList);
+    }
+
+    public IActionResult Details(int id)
+    {
+        Product product = _unitOfWork.ProductRepository.Get(x=>x.Id == id,includeProperties: "Category");
+        return View(product);
     }
 
     public IActionResult Privacy()
